c4a7cecfee5c1e3bccfcf9d027c7ebabc8b47ecf
mattlim96-MacBook Pro <mattlim96@gmail.com>
2021-10-14 15:12:20 +0100

    Update class1_ex.R

--- class1_ex.R
+++ class1_ex.R
@@ -2,3 +2,23 @@ y <- c(8,5,7,5,6,3,5,4,2,2)
 x <- c(1.3,2.9,4.4,5.8,8.2,9.9,12.1,13.8,14.2,17.7)
 y
 x
+
+m <- 1:10
+m
+
+f <- function() {
+  ## This is an empty function
+}
+class(f)
+
+x <- vector("numeric", length = 10)
+x
+
+f <- function(a, b) {a^2}
+f(2)
+
+f <- function(a, b) {
+      print(a)
+      print(b)
+     }
+f(45)
